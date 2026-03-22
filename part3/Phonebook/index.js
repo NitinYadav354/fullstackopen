ea1abb205b4bb5642084e399ecb8a6b0f82af1e8
Nitin Yadav <nyadav4625@gmail.com>
2026-03-22 22:46:07 +0530

    Exercise 3.9-3.11

--- part3/Phonebook/index.js
+++ part3/Phonebook/index.js
@@ -4,7 +4,11 @@ const app = express()
 const morgan = require('morgan')
 const cors = require('cors')
 
-app.use(cors())
+app.use(cors({
+  origin: '*',
+  methods: 'GET,POST,PUT,DELETE',
+  allowedHeaders: 'Content-Type',
+}))
 
 const Data = [
     { 
